3228b135eb33d79210bdcaca614b86f33062681d
kobishapira <78294641+kobishapira@users.noreply.github.com>
2023-02-23 16:18:01 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,5 +48,5 @@ my_cur = my_cnx.cursor()
 my_cur.execute("select * from PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST")
 my_data_row = my_cur.fetchone()
 streamlit.text("The fruit load list contains:")
-streamlit.text(my_data_raw)
+streamlit.text(my_data_row)
 
