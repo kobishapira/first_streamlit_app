94a4ceab700d646e5a17c2331c1c965025730ff3
kobishapira <78294641+kobishapira@users.noreply.github.com>
2023-03-01 10:06:02 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,18 +29,26 @@ streamlit.dataframe(fruits_to_show )
 
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','apple')
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    strealit.error("Please select a fruit to get info")
+  else
+  #import requests
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+  # write your own comment -what does the next line do? 
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  # write your own comment - what does this do?
+streamlit.dataframe(fruityvice_normalized)
+    
+  
+ except URLError as e:
+  strealit.error()
+  
+  
 streamlit.write('The user entered ', fruit_choice)
 
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
-
-
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
 
 #import snowflake.connector
 
